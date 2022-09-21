63e676f5a3063377a49c94a4fd863acb6b3b2836
jtzuya <jtzuya@gmail.com>
2022-09-21 21:44:44 +0800

    EJS environment Setup
    Setting up EJS base environment as well as express routing (reuseable)

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 
 # Type of Module helpful Blog (ES6)
 ~ https://stackoverflow.com/questions/61401475/why-is-type-module-in-package-json-file
+
+### EJS
+~ change the files names inside ui folder
+Example: 
+directiory [/view/index.html]
+index.html ===> index.ejs
+
+### How to clone the repo and make it your own
+~ git clone [HTTPS] or [SSH]
+~ git pull (if you want to pull this repo)
+~ git remote -v (to view existing remotes)
+~ git remote set-url origin [your repository HTTPS or SSH]
--- app.js
+++ app.js
@@ -8,9 +8,34 @@ app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
 app.use('/assets', express.static(__dirname + 'public/assets'))
 
+// Initialize EJS (check guide at README.md)
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
 // Always do the params in order. req first, then res last.
 app.get('', (req, res) => {
-    res.sendFile(__dirname + '/ui/index.html')
+    // res.sendFile(__dirname + '/ui/index.html')
+
+    // Instead of sending the file. use render (ejs)
+    // text & text2 are variables that can be used inside the ejs file
+    res.render('index', { 
+        text: 'This is a sample text variable (templating engine)',
+        text2: 'smooth'
+    })
+})
+
+app.get('/about', (req, res) => {
+    // text is a variable that can be used inside the ejs file
+    res.render('about', {
+        text: 'about page'
+    })
+})
+
+app.get('/home', (req, res) => {
+    // text is a variable that can be used inside the ejs file
+    res.render('homepage', {
+        text: 'home page'
+    })
 })
 
 // Listen
